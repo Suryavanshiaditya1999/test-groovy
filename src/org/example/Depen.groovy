9dfb6d3b97b3731a413eefb55d28685ab5acfef3
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-12 01:22:36 +0530

    Update Depen.groovy

--- src/org/example/Depen.groovy
+++ src/org/example/Depen.groovy
@@ -1,18 +1,20 @@
 package org.example
 
-def call() {
-    def workspaceDir = "${WORKSPACE}"
-    echo "Current directory: ${pwd()}"
-    echo "Workspace directory: ${workspaceDir}"
-    sh """
-        ls -la ${workspaceDir}
-        python3 -m venv myenv
-        . myenv/bin/activate
-        echo "Workspace directory: ${workspaceDir}"
-        pip install -r ${workspaceDir}/requirments.txt
-        pip install coverage
-        pip install pytest
-        coverage run -m pytest
-        coverage report -m
-    """
+class Depen {
+    void call() {
+        def workspaceDir = "${WORKSPACE}"
+        println "Current directory: ${pwd()}"
+        println "Workspace directory: ${workspaceDir}"
+        sh """
+            ls -la ${workspaceDir}
+            python3 -m venv myenv
+            . myenv/bin/activate
+            echo "Workspace directory: ${workspaceDir}"
+            pip install -r ${workspaceDir}/requirements.txt
+            pip install coverage
+            pip install pytest
+            coverage run -m pytest
+            coverage report -m
+        """
+    }
 }
