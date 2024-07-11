6be6885a0209caf849032334228bac1100ba3f35
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-12 02:12:37 +0530

    Update Depen.groovy

--- src/org/example/Depen.groovy
+++ src/org/example/Depen.groovy
@@ -1,19 +1,3 @@
-package org.example
-
-class Depen {
-    void setupAndRunTests(Map<String, String> env, String workspaceDir) {
-        // Now you can access the env object directly
-        echo "Current directory: ${env.WORKSPACE}"
-        echo "Workspace directory: ${workspaceDir}"
-        
-        // Your existing setup and test execution commands...
-        sh "ls -la ${workspaceDir}"
-        sh "python3 -m venv myenv"
-        sh ". myenv/bin/activate"
-        sh "pip install -r ${workspaceDir}/requirements.txt"
-        sh "pip install coverage"
-        sh "pip install pytest"
-        sh "coverage run -m pytest"
-        sh "coverage report -m"
-    }
+def call() {
+    sh 'echo " hello world " '
 }
