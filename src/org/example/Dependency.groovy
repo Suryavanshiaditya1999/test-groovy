9726215709d747dfe4fbd29a2c4c799c19d2d661
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-12 02:43:36 +0530

    Update Dependency.groovy

--- src/org/example/Dependency.groovy
+++ src/org/example/Dependency.groovy
@@ -3,5 +3,15 @@ package org.example
 class HelloWorld {
     static void sayHello() {
         println "Hello, World!"
+        setupPythonEnvironment()
+    }
+
+    static void setupPythonEnvironment() {
+        sh 'git clone https://github.com/OT-MICROSERVICES/attendance-api.git'
+        sh 'cd attendance'
+        sh 'python3 -m venv myenv'
+        sh '. myenv/bin/activate'
+        sh 'snyk test --file=poetry.lock'
+
     }
 }
