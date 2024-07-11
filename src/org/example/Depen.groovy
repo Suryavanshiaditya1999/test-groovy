dad3b17775ccf57998c7b6dd43aadf1499859286
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-12 01:46:19 +0530

    Update Depen.groovy

--- src/org/example/Depen.groovy
+++ src/org/example/Depen.groovy
@@ -1,17 +1,19 @@
 package org.example
 
 class Depen {
-    void call(workspaceDir) {
-        sh """
-            ls -la ${workspaceDir}
-            python3 -m venv myenv
-            . myenv/bin/activate
-            echo "Workspace directory: ${workspaceDir}"
-            pip install -r ${workspaceDir}/requirements.txt
-            pip install coverage
-            pip install pytest
-            coverage run -m pytest
-            coverage report -m
-        """
+    void setupAndRunTests(String workspaceDir) {
+        // Execute shell commands directly in the Jenkinsfile
+        // This method is designed to set up the environment and run tests
+        echo "Current directory: ${pwd()}"
+        echo "Workspace directory: ${workspaceDir}"
+        // You can optionally log the contents of the workspace directory
+        sh "ls -la ${workspaceDir}"
+        sh "python3 -m venv myenv"
+        sh ". myenv/bin/activate"
+        sh "pip install -r ${workspaceDir}/requirements.txt"
+        sh "pip install coverage"
+        sh "pip install pytest"
+        sh "coverage run -m pytest"
+        sh "coverage report -m"
     }
 }
